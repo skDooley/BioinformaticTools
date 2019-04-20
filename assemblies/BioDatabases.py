41c5a3b9b6849d2401353a75a2243a81e8a6d7c0
dooleys1 <dooley@iastate.edu>
2019-04-19 21:20:09 -0400

    Updated NCBI database downloader to python 3

--- assemblies/BioDatabases.py
+++ assemblies/BioDatabases.py
@@ -6,152 +6,175 @@ from ftplib import FTP
 from csv import DictReader
 from pickle import dump, load
 class database_source(object):
-    def __init__(self,name):
+    def __init__(self,name,direction=True,fraction=1.0):
         self.name = name
         self.currentFiles = {}
         self.newFiles = {}
         self.ftp = None
         self.failed = {}
+        theDate = str(datetime.date.today())
+        logFileName = "%s_download_error_%s.log" % (name,theDate)
+        count = 1
+        while (os.path.exists(logFileName)):
+            logFileName = "%s_download_error_%s.log%i" % (name,theDate,count)
+            count += 1
+        self.logFileName = logFileName
+        self.logfh = open(logFileName,"w")
+        self.dir = direction #For large databases this option allows multiple downloaders to run at once and not collide
+        self.fraction = fraction #For large databases this option allows you to start at a particular percentage of the way through 
+        #the list of files from that particular database.
+    
     def update(self):
-        self._get_new_manifest()
-        self._get_current_file_state()
-        self._get_new_assemblies()
-        try: print "Finished update"
-        except: print "In Error"
-        finally: 
-            print "Got the assembly now time to pickle"
-            dump(self.currentFiles,open("%s_currentAssemblies.p" % (self.name),"wb"))
-            theDate = str(datetime.date.today())
-            if len(self.failed)>0: 
-                for val in self.failed:
-                    print ("Failed:",val)
-                dump(self.failed,open("%s_failedAssemblies_%s.p" % (self.name,theDate), "wb"))
-            #TODO notify user is assemblies failed
-            self.close() #Close FTP conection
-        if len(self.failed)>0:
-            print ("Failed:",len(self.failed))
-            dump(self.failed,open("%s_failedAssemblies_%s.p" % (self.name,theDate), "wb"))         
+        try:
+            self._get_new_manifest()
+            self._get_current_file_state()
+            self._get_new_assemblies()
+            self._logFailures()
+            print ("Finished downloading assemblies. Please see the log file %s for download information if there was a failure." % (self.logFileName))
+            self.close() #Close FTP conection 
+        except Exception as e:
+            self._logFailures()
+            self.close()
+            raise e
+    
+    def log(self, msg): self.logfh.write(msg+"\n")
+    
+    def _logFailures(self): 
+        if len(self.failed)==0:return
+        for source in self.failed:
+            for domain in self.failed[source]:
+                if len(self.failed[source][domain]) > 0: self.log("Failed to download the following assemblies from %s %s:" % (source,domain))
+                for assemName in self.failed[source][domain]: self.log("\t" + assemName)
+        
+        dump(self.failed,open("%s_failedAssemblies_%s.p" % (self.name,theDate), "wb"))
+
     def close(self):
+        self.logfh.close()
         if self.ftp != None: self.ftp.close()
         if len(self.cleanupFiles)>0: os.system("rm -f %s" % (" ".join(self.cleanupFiles)))
 
-def ftp_exists(ftp,ftpFilePath,ftpDir):
-    try:
-        ftp.nlst(ftpFilePath)
-        return ftpFilePath
-    except:
-        files = ftp.nlst(ftpDir)
-        for file in files:
-            if "_cds_" in file or "_rna_" in file:continue
-            if "_genomic.fna.gz" in file: return file
-        return None            
+    def ftp_exists(self,ftpFilePath,ftpDir):
+        try:
+            self.ftp.nlst(ftpFilePath)
+            return ftpFilePath
+        except:
+            files = self.ftp.nlst(ftpDir)
+            for file in files:
+                if "_cds_" in file or "_rna_" in file: continue
+                if "_genomic.fna.gz" in file: return file
+            return None            
 
-def checkFilePath(ftp,filePath,ftpFile):
-    #If both the fasta and gz file exist:
-    #print "Looking for:",ftpFile
-    if os.path.exists(filePath) and os.path.exists(filePath.replace(".gz","")):
-        print ("Removing",filePath,filePath.replace(".gz",""))
-        os.system("rm %s %s" % (filePath,filePath.replace(".gz","") ))
-        return True
-    #If only 1 of the files exist
-    if os.path.exists(filePath) or os.path.exists(filePath.replace(".gz","")): #check the the fasta or the fasta.gz exists
-        #GZ file that exists
-        if os.path.exists(filePath):
-            try: fileSize = os.stat(filePath).st_size
-            except:fileSize = 0
-            if fileSize == 0 or fileSize < ftp.size(ftpFile): 
-                print ("Removing", filePath, "Disk Size:",fileSize, "FTP Size:", ftp.size(ftpFile))
-                os.system("rm " + filePath ) #If GZ is zero or less than the size on the ftp
-            else: 
-                print("These should be the same",ftp.size(ftpFile), fileStats.st_size,filePath, ftp.size(ftpFile)==fileStats.st_size)
-                sys.exit(0)
-                return ftp.size(ftpFile)==fileStats.st_size
+    def fastaExists(self,filePath,ftpFile):
+        #If both the fasta and gz file exist:
+        if os.path.exists(filePath) and os.path.exists(filePath.replace(".gz","")):
+            os.system("rm %s" % (filePath)) #remove the gzipped file
             return True
-        #Fasta exists
+        #another version of the downloader already downloaded it
+        if os.path.exists(filePath.replace(".gz","")): 
+            #Check that the file was fully downloaded
+            try: fileSystemSize = os.stat(filePath).st_size
+            except: fileSystemSize = 0
+            if fileSize == 0 or fileSystemSize < self.ftp.size(ftpFile): 
+                self.log("Removing", filePath, "Disk Size:",fileSize, "FTP Size:", ftp.size(ftpFile))
+                os.system("rm " + filePath )
+                return False
+            return True 
         return False
-    return True
 
-
-    
 class NCBI(database_source):
-    def __init__(self,sources=set(['genbank','refseq']),downloadPath="./"):
+    def __init__(self, sources=['refseq'], domains=['bacteria'], downloadPath="./NCBI", direction=True, fraction=1.0):
         database_source.__init__(self,"NCBI")
-        self.ftp = None #FTP('ftp.ncbi.nlm.nih.gov','anonymous','wangqion@gmail.com')
+        self.ftp = None
         self.cleanupFiles = []
-        self.domains = set(['archaea']) #,'plasmid','viral'])
-        self.sources = sources
+        self.domains = domains # ['bacteria', 'archaea', 'plasmid', 'viral']
+        self.sources = sources # ['genbank',  'refseq'                     ]
         self.downloadPath = downloadPath
-    def get_database(self):
-        self._get_new_manifest()
-        self._get_current_file_state()
-        self._get_new_assemblies()
+        self.dir = 1
+        if not direction: self.dir = -1
+        self.fraction = fraction
+        if (fraction > 1.0) or (fraction <= 0.0):
+            eprint("The fraction must be between 0 and 1. Exiting run.")
+            sys.exit(-1)
+
+    def get_database(self): self.update()
+
     def _get_new_manifest(self):
-        print "Getting newest NCBI manifests"
+        print("Getting newest NCBI manifests from", " and ".join(self.sources))
         for source in self.sources:
             self.newFiles[source] = {}
             for domain in self.domains:
                 self.newFiles[source][domain] = {}
                 theDate = str(datetime.date.today())
                 manifest = "%s/%s_%s_assembly_summary_%s.txt" % (self.downloadPath , domain, source, theDate)
-                cmd = "wget ftp://ftp.ncbi.nlm.nih.gov/genomes/%s/%s/assembly_summary.txt -O %s" % (source.lower(),domain,manifest)
-                os.system(cmd)
-                print ("\t",cmd)
+                cmd = "wget ftp://ftp.ncbi.nlm.nih.gov/genomes/%s/%s/assembly_summary.txt -O %s 2>/dev/null" % (source.lower(),domain,manifest)
+                print("\tGetting:", manifest)
+                #os.system(cmd)
                 file = open(manifest,"r")
                 file.readline()
                 for rec in DictReader(file,delimiter="\t"): self.newFiles[source][domain][rec['# assembly_accession']] = rec
-    def connect(self,source,domain): 
-        if self.connected(): self.close()
-        self.ftp = FTP('ftp.ncbi.nlm.nih.gov','anonymous','wangqion@gmail.com')
-        cwd = "genomes/%s/%s" %(source.lower(),domain)
-        self.ftp.cwd(cwd)
+                print("\t\t%i assemblies in %s %s" % (len(self.newFiles[source][domain]),source,domain))
 
-    def connected(self):return self.ftp != None 
+    def connect(self,source,domain): 
+        if self.connected(): self.close() #If already connected then close the current connection to reset the connection
+        self.ftp = FTP('ftp.ncbi.nlm.nih.gov','anonymous','wangqion@gmail.com')
+        self.ftp.cwd("genomes/%s/%s" %(source.lower(),domain))
+        self.logfh = open(self.logFileName,"a") #Reopen the error log file
+
+    def connected(self): return self.ftp != None 
+
     def _get_current_file_state(self):
-        self.currentFiles = {'refseq':{'bacteria':set(),'archaea':set()},'genbank':{'bacteria':set(),'archaea':set()}} #{} #load(open("%s_currentAssemblies.p" % (self.name),"rb"))
-        print ("Number of assemblies all ready downloaded:")
-        for source in self.sources:
-            for domain in self.domains: 
-                for fileName in os.listdir("/mnt/research/germs/shane/databases/assemblies/NCBI/%s/%s" % (source,domain)):
-                    self.currentFiles[source][domain].add(fileName.replace(".fasta",""))
-                print ("\t%s in %s: %i" % (domain,source,len(self.currentFiles[source][domain])))
-  
-    def _get_new_assemblies(self):
-        print ("Getting new assemblies")
+        #if (os.path.exist()): load(open("%s_currentAssemblies.p" % (self.name),"rb"))
+        self.currentFiles = {'refseq':{'bacteria':set(),'archaea':set()},'genbank':{'bacteria':set(),'archaea':set()}}
+        print ("Number of assemblies already downloaded:")
         for source in self.sources:
             for domain in self.domains:
-                if len(self.newFiles[source][domain]) > len(self.currentFiles[source][domain]): 
-                    assembliesToGet = list(set(self.newFiles[source][domain]).difference(self.currentFiles[source][domain]))
-                    
-                    print ("Number to get:", len(assembliesToGet), len(self.currentFiles[source][domain]), len(self.newFiles[source][domain]))
-                    self.connect(source.lower(),domain)
-                    #TODO In the future need to look at self.newFiles[source][domain][assembly]['seq_rel_date'] to determine if assembly needs to be replaced
-                    halfway = int(len(assembliesToGet)/2.0)
-                    quarterway = halfway/2
-                    count = len(assembliesToGet)
-                    totalLen = len(assembliesToGet)
-                    print (source,domain, "Assemblies to download:",totalLen,"/", len(self.newFiles[source][domain]))
-                    for assembly in assembliesToGet[::-1]: #[quarterway+halfway::-1]:
-                        try:
-                            filePath = "NCBI/%s/%s/%s.fasta.gz" %(source,domain,assembly)
-                            ftpDir = self.newFiles[source][domain][assembly]['ftp_path'].replace("ftp://ftp.ncbi.nlm.nih.gov","")
-                            ftpFile = "%s/%s_%s_genomic.fna.gz" % (ftpDir,assembly,self.newFiles[source][domain][assembly]['asm_name'].replace(" ","_").replace("#","_"))
-                            if checkFilePath(self.ftp,filePath,ftpFile):
-                                ftpFile = ftp_exists(self.ftp,ftpFile,ftpDir)
-                                self.ftp.retrbinary('RETR %s' % (ftpFile) , open(filePath,'wb').write)
-                                os.system("gunzip -q -f -d %s" % filePath)
-#                                 if count % 3 == 0: time.sleep(1)
-                            self.currentFiles[source][domain][assembly] = self.newFiles[source][domain][assembly] #Add the assembly to assemblies completed
-#                             count -= 1
-#                             if count % 1000 == 0:print "Completed downloading",totalLen-count, "of", totalLen
-                        except Exception, e:
-                            os.system("rm -f "+filePath)
-                            self.close()
-                            time.sleep(10)
-                            self.connect(source,domain)
-                            ftpFile = ftp_exists(self.ftp,ftpFile,ftpDir)
-                            print ("Failed",source,domain,assembly,ftpFile)
-                            if ftpFile != None:
-                                self.ftp.retrbinary('RETR %s' % (ftpFile) , open(filePath,'wb').write)
-                                os.system("gunzip -q -f -d %s" % filePath)
-                                #print "Completed",source,domain,assembly
-                                count -= 1
+                dirFiles = os.listdir(os.path.join(self.downloadPath,source,domain))
+                for fileName in dirFiles: self.currentFiles[source][domain].add(fileName.replace(".fasta",""))
+                print ("\t%i already downloaded from %s %s" % (len(self.currentFiles[source][domain]),source,domain))
+  
+    def _get_new_assemblies(self):
+        print ("Getting undownloaded assemblies")
+        for source in self.sources:
+            for domain in self.domains:
+                if len(self.newFiles[source][domain]) <= len(self.currentFiles[source][domain]): continue
+                assembliesToGet = list(set(self.newFiles[source][domain]).difference(self.currentFiles[source][domain]))
+                self.connect(source.lower(),domain)
+                #TODO: Look at self.newFiles[source][domain][assembly]['seq_rel_date'] to determine if assembly needs to be replaced
+                startIndex = 0
+                endIndex = len(assembliesToGet)
+                if(self.fraction != 1.0): startIndex = int(endIndex*self.fraction)
+                if not self.dir:
+                    startIndex = endIndex
+                    endIndex = 0
+                print("\tGetting %i assemblies from %s %s." % (abs(endIndex-startIndex),source,domain))
+                count = 0
+                for assembly in assembliesToGet[startIndex:endIndex:self.dir]:
+                    filePath = "%s/%s/%s/%s.fasta.gz" %(self.downloadPath,source,domain,assembly)
+                    ftpDir = self.newFiles[source][domain][assembly]['ftp_path'].replace("ftp://ftp.ncbi.nlm.nih.gov","")
+                    ftpFile = "%s/%s_%s_genomic.fna.gz" % (ftpDir,assembly,self.newFiles[source][domain][assembly]['asm_name'].replace(" ","_").replace("#","_"))
+                    try:
+                        if not self.fastaExists(filePath,ftpFile):
+                            ftpFile = self.ftp_exists(ftpFile,ftpDir)
+                            if not ftpFile: raise(Exception("File is not accessable or the ftp failed"))
+                            self.ftp.retrbinary('RETR %s' % (ftpFile) , open(filePath,'wb').write)
+                            os.system("gunzip -q -f -d %s" % filePath)
+                            count += 1
+                            if ((count % 1000) == 0): print("\t\tCompleted %i assemblies of %i" % (count,len(assembliesToGet[startIndex:endIndex])))
+                        self.currentFiles[source][domain] = assembly #Add the assembly to assemblies completed
+                    except Exception as e:
+                        os.system("rm -f "+filePath)
+                        self.close()
+                        time.sleep(3) # sleep 3 seconds after a failure
+                        self.connect(source,domain)
+                        ftpFile = self.ftp_exists(ftpFile,ftpDir)
+                        
+                        if ftpFile != None:
+                            self.ftp.retrbinary('RETR %s' % (ftpFile) , open(filePath,'wb').write)
+                            os.system("gunzip -q -f -d %s" % filePath)
+                            count+=1
+                        else:
+                            badFile = assembly+" "+ftpDir
+                            try:self.failures[source][domain].append(badFile)
+                            except:
+                                try: self.failures[source][domain] = [badFile]
+                                except: self.failures[source] = {domain:[badFile]}
--- assemblies/GetNCBI.py
+++ assemblies/GetNCBI.py
@@ -1,14 +1,39 @@
 from BioDatabases import NCBI
-import time
-import os
-mulipleTries = 0
+from time import sleep
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-f", "--fraction",  dest="FRAC", default=1.0, help="The fraction starting point of the array")
+parser.add_option("-d", "--direction", dest="DIR", action="store_false", default=True, 
+	                                   help="The direction to go in the array (backwards = False, forwards = True)")
+
+parser.add_option("-r", "--refseq",   dest="REFSEQ", action="store_true", default=False,  help="Download from RefSeq")
+parser.add_option("-g", "--genbank",  dest="GB",     action="store_true",  default=False, help="Download from GenBank")
+parser.add_option("-b", "--bacteria", dest="BACT",   action="store_true", default=False,  help="Download bacterial assemblies")
+parser.add_option("-a", "--archaea",  dest="ARCH",   action="store_true",  default=False, help="Download archaeal assemblies")
+
+(opts, args) = parser.parse_args()
+
+#Download parameters
+sleepTime = 3*60
+downloadPath = "/mnt/research/germs/shane/databases/assemblies/NCBI"
+sources = []
+if opts.GB: sources.append("genbank")
+if opts.REFSEQ: sources.append("refseq")
+
+domains = []
+if opts.BACT:domains.append("bacteria")
+if opts.ARCH:domains.append("archaea")
+
+
+#Run the downloader
+attempt = 1
 while True:
 	try:
-		downloadPath = "NCBI/genbank"
-		getData = NCBI(["genbank"],downloadPath)
-		getData.get_database()
+		downloader = NCBI(sources,domains,downloadPath, fraction=float(opts.FRAC), direction=opts.DIR)
+		downloader.get_database()
 	except:
-		time.sleep(3*60)
-		files = os.listdir("/mnt/research/germs/shane/databases/assemblies/NCBI/refseq/bacteria")
-		mulipleTries += 1
-		if mulipleTries >= 4: break
+		if attempt == 4: break
+		print("Download attempt %i failed. Sleeping for %i minutes, then retrying." % (attempt,sleepTime))
+		sleep(sleepTime)
+		attempt += 1
